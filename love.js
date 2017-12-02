d2d9f30ce005c1c38ba6f10118aa2f3e6ce62293
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2017-12-01 14:17:32 -1000

    Update 2

--- love.js
+++ love.js
@@ -2,39 +2,122 @@ var hero = "Keegan"; //b
 var gender = "MD Helicopters MH-6 Little Bird"; //x
 var age = 21; //y
 var weapon = "Gewehr 98"; //z
-var pronoun = "He"; //a
+var pronoun = ""; //a
 
-function MyGender(x){ //Settig the Gender
-if (x === "MD Helicopters MH-6 Little Bird" || x === "Male"){
-	pronoun = "He";
+ //Settig the Gender
+if (gender == "MD Helicopters MH-6 Little Bird" || gender == "Male"){
+	pronoun ="His";
 }
-	else if (x === "M1 Abrams" || x === "Female"){
-		pronoun = "She";
+	else if (gender == "M1 Abrams" || gender == "Female"){
+		pronoun ="Her";
 	}
 		else{
-			pronoun = "Unknown";
+			pronoun ="Undefined";
 		}
-}
+
 MyGender(gender);
-console.log(pronoun);
 
-
-function MyAge(y){ //Setting the Age
-if (y < 18){
+//Setting the Age
+if (age < 18){
 	output = "is just beginning";
 }
 
-	else if (y < 21 && 40){
+	else if (age > 21 && y < 40){
 		output = "has seen a few battles";
 	}
 
-	else if (y > 40){
+	else if (age > 40){
 		output = "has lived through many epic battles";
 	}
-}
-MyAge(age);
-console.log(output);
 
-//function Quote(b, y, z){
-	//return = 
-//}
+MyAge(age);
+console.log(hero + "is our lovable hero, who" + age == 21 + "with his trusty" + weapon);
+
+
+
+
+
+
+
+
+
+
+
+var xp;
+ var hero = "Jake";
+ var gender = "male";
+ var age = 36;
+ var weapon = "laser";
+ var pronoun;
+
+
+if (gender == "male") {
+ pronoun = "his";
+ } else if (gender == "female") {
+ pronoun = "her";{
+}
+
+if (age < 18){
+ xp = "is just the beggining";
+}
+ else if (age > 21 && age <40) {
+   xp = "has seen a few battles";
+ }
+ else if (age > 40) {
+   xp = "has lived through many epic battles";
+ }
+
+
+
+
+console.log(hero + "is our lovable hero, who" + age == 25 + "with his trusty" + weapon);
+
+
+var count;
+var animal = "koala";
+function animalHouse(a,b){
+ count = a+b;
+ console.log(count);
+}
+animalHouse(10,23);
+
+
+if (count > 2) {
+ console.log("We have to many " + animal + "s")
+
+var hero = "hulk";
+function avengersAssemble(a){
+ if (a == "hulk") {
+   console.log("hulk smash");
+ } else if (a == "ironman"){
+   console.log("suit up");
+ } else if (a == "hawkeye"){
+   console.log("bulls eye");
+ } else if (a == "captain america"){
+   console.log("clean up");
+ }
+}
+avengersAssemble(hawkeye);
+
+var light =true;
+Boolean (light);
+if (light == true) {
+    console.log('And then there was light');
+} else {
+    console.log('Lights out');
+}
+
+var carType = ["Toyota", "Lamborghini", "Tesla"];
+var color = ["red", "blue", "black"];
+var num = 12;
+if (carType == "Toyota" && color == "blue"){
+ console.log("we have " + num + " " + color [1] + " " + carType [0]);
+ } if (carType == "Lamborghini" && color == "blue"){
+   console.log("we have " + num + " " + color [0] + " " + carType [1]);
+ } if (carType == "Tesla" && color == "black"){
+   console.log("we have " + num + " " + color [2] + " " + carType [2]);
+ }else {
+   console.log("We dont have any in stock");
+}
+
+
